af7465a0b3c083e28cea1d16075cb2912e84e1a6
승혁 오 <asd7726191@naver.com>
2019-06-18 21:47:27 +0900

    커밋

--- 오승/github.c
+++ 오승/github.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
+#include<string.h>
 
 void encrypt(char buf[], int shift);
 
 int main(void)
 {
-	FILE *fp;
-	fp = fopen("abc.txt","w+");
+	char filename[100];
+	char savefile[100];
 	char buf[100];
-	char a[100] = { "Hello World" };
+	char b[100] = "";
+	char a[100] = { "Hello World\nSecond Line" };
+	printf("파일 이름을 입력:");
+	scanf("%s", filename);
+	FILE *fp;
+	fp = fopen(filename,"w+");
 	fputs(a, fp);
 	fclose(fp);
-	fopen("abc.txt", "r");
-	fgets(buf, 100, fp);
-	fclose(fp);
-	
-	fp = fopen("s.enc.txt", "w+");
+	printf("저장할 파일 이름을 입력:");
+	scanf("%s", &savefile);
 	int shift;
 	printf("shift=");
 	scanf("%d", &shift);
-	encrypt(buf, shift);
-	fputs(buf, fp);
-	
+	fp = fopen(filename, "r");
+	while(!feof(fp))
+	{
+		fgets(buf, 100, fp);
+		
+		encrypt(buf, shift);
+		strcat(b, buf);
+	}
+	fclose(fp);
+
+	fp = fopen(savefile, "a+");
+	fputs(b, fp);
 	fclose(fp);
 }
 void encrypt(char buf[], int shift) {
 	int i = 0;
 
 
-	while (buf[i] != '\0') 
+	while (buf[i] !='\0') 
 	{
 		if (buf[i] >= 'A' && buf[i] <= 'z') 
 		{
